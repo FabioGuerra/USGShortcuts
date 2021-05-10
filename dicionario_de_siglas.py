12acee5e39659b24c84cf3467c16a160403d1374
Fabio Rodrigues Guerra <fabioguerrra@hotmail.com>
2021-05-10 08:30:53 -0300

    Update dicionario_de_siglas.py
    Fixed: Atualizar siglas

--- dicionario_de_siglas.py
+++ dicionario_de_siglas.py
@@ -1,9 +1,10 @@
 import database
 
 db = database.Database('siglas.db')
-dicio_siglas = {}
 
-for row in database.Database.view(db):
-    dicio_siglas[row[1]] = row[2]
+def siglas():
+    dicio_siglas = {}
+    for row in database.Database.view(db):
+        dicio_siglas[row[1]] = row[2]
 
-db.conn.close()
+    return dicio_siglas
